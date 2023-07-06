cdc04d27b59859bb92038fc5e8363716295b2c7d
Mattias Michaux <mattias.michaux@dropsolid.com>
2023-07-06 15:04:12 +0200

    re-add missing commit to fix incorrect reporting dates

--- app/bundles/ReportBundle/Views/Report/details_data.html.php
+++ app/bundles/ReportBundle/Views/Report/details_data.html.php
@@ -114,7 +114,7 @@ $graphContent = $view->render(
                                         <td>
                                             <?php $closeLink = false; ?>
                                             <?php if (isset($columns[$key]['link']) && !empty($row[$columns[$key]['alias']])): ?>
-                                        <?php $closeLink = true; ?>
+                                                <?php $closeLink = true; ?>
                                             <a href="<?php echo $view['router']->path($columns[$key]['link'], ['objectAction' => 'view', 'objectId' => $row[$columns[$key]['alias']]]); ?>" class="label label-success">
                                                 <?php endif; ?>
                                                 <?php
@@ -129,18 +129,20 @@ $graphContent = $view->render(
                                                         // For grouping by datetime fields, so we don't get the timestamp on them
                                                         if ('datetime' === $cellType && 10 === strlen($cellVal)) {
                                                             $cellType = 'date';
-                                                        }
+                                                         }
 
-                                                        switch ($cellType) {
-                                                            case 'datetime':
-                                                                echo $view['date']->toFullConcat($cellVal, 'UTC');
-                                                                break;
-                                                            case 'date':
-                                                                echo $view['date']->toShort($cellVal, 'UTC');
-                                                                break;
-                                                            default:
-                                                                echo $view['formatter']->_($cellVal, $cellType);
-                                                                break;
+                                                        if ('' !== $cellVal && !is_null($cellVal)) {
+                                                            switch ($cellType) {
+                                                                case 'datetime':
+                                                                    echo $view['date']->toFullConcat($cellVal, 'UTC');
+                                                                    break;
+                                                                case 'date':
+                                                                    echo $view['date']->toShort($cellVal, 'UTC');
+                                                                    break;
+                                                                default:
+                                                                    echo $view['formatter']->_($cellVal, $cellType);
+                                                                    break;
+                                                            }
                                                         }
                                                     }
                                                 ?>
